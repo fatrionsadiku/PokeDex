6d8dafdd47de85df40716ce787040c3bfa44d1d2
FatrionSadiku <fatrion.sa@gmail.com>
2023-03-15 00:47:21 +0100

    Fixed the laggy UI and also the livedata population issue.

--- app/src/main/java/com/example/pokedex/ui/HomeFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/HomeFragment.kt
@@ -6,41 +6,37 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokedex.adapters.PokeAdapter
-import com.example.pokedex.data.models.Pokemon
 import com.example.pokedex.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-    lateinit var binding : FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
     private val viewModel by lazy {
         ViewModelProvider(this)[HomeViewModel::class.java]
     }
     private val recyclerView by lazy {
         binding.recyclerView
     }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.getPaginatedPokemons(20)
         val adapter = PokeAdapter()
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
-            viewModel.pokemon.observe(viewLifecycleOwner){
-                adapter.pokemons = it
-            }
+        recyclerView.layoutManager = GridLayoutManager(requireContext(),2)
+        viewModel.pokemon.observe(viewLifecycleOwner) {
+            adapter.pokemons = it
         }
-
-
     }
 }
--- app/src/main/java/com/example/pokedex/ui/HomeViewModel.kt
+++ app/src/main/java/com/example/pokedex/ui/HomeViewModel.kt
@@ -7,25 +7,21 @@ import androidx.lifecycle.viewModelScope
 import com.example.pokedex.data.models.Pokemon
 import com.example.pokedex.data.models.PokemonsApiResult
 import com.example.pokedex.data.server.Retrofit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import retrofit2.awaitResponse
 
 class HomeViewModel : ViewModel() {
-    val pokemon: MutableLiveData<MutableList<Pokemon>> = MutableLiveData()
-    val paginatedPokemon = MutableLiveData<PokemonsApiResult>()
+    val pokemon: MutableLiveData<MutableList<Pokemon?>> = MutableLiveData()
 
 
-
-//    init {
-//        viewModelScope.launch {
-//            getPaginatedPokemons(20)
-//        }
-//    }
-
-    fun getPaginatedPokemons(limit: Int) = viewModelScope.launch {
+    fun getPaginatedPokemons(limit: Int)  {
+        val pokeList = mutableListOf<Pokemon?>()
         val call = Retrofit.pokeApi.getPaginatedPokemons(limit)
         call.enqueue(object : Callback<PokemonsApiResult?> {
             override fun onResponse(
@@ -33,18 +29,25 @@ class HomeViewModel : ViewModel() {
                 response: Response<PokemonsApiResult?>
             ) {
                 if (response.isSuccessful) {
-                    val pokeList = mutableListOf<Pokemon>()
-                    response.body()?.let { paginatedPokeResponse ->
-                        paginatedPokeResponse.results.forEach {
-                            val pokeId = it.url.replace(
-                                "https://pokeapi.co/api/v2/pokemon/",
-                                ""
-                            ).replace("/", "").toInt()
-                            getPokemon(pokeId, pokeList)
+                    viewModelScope.launch {
+                        response.body()?.let { paginatedPokeResponse ->
+                            paginatedPokeResponse.results.forEach { Poke ->
+                                val pokeId = Poke.url.replace(
+                                    "https://pokeapi.co/api/v2/pokemon/",
+                                    ""
+                                ).replace("/", "").toInt()
+                                val currentPoke = async(Dispatchers.IO){
+                                    getPokemon(pokeId)
+                                }
+                                currentPoke.await()?.let {
+                                    pokeList.add(it)
+                                }
+                            }
+                            pokemon.postValue(pokeList)
                         }
+                        Log.d("PokeList", pokeList.toString())
                     }
-                    Log.d("PokeList", pokeList.toString())
-                    pokemon.postValue(pokeList)
+
                 }
 
             }
@@ -55,21 +58,20 @@ class HomeViewModel : ViewModel() {
         })
     }
 
-    fun getPokemon(number: Int, pokeList: MutableList<Pokemon>) = viewModelScope.launch {
-        val call = Retrofit.pokeApi.getPokemons(number)
-        call.enqueue(object : Callback<Pokemon?> {
-            override fun onResponse(call: Call<Pokemon?>, response: Response<Pokemon?>) {
-                if (response.isSuccessful) {
-                    Log.d("PokeResponse", response.body().toString())
-                    response.body()?.let {
-                        pokeList.add(it)
-                    }
-                }
+    suspend fun getPokemon(number: Int): Pokemon? {
+        return try {
+            val call = Retrofit.pokeApi.getPokemons(number)
+            val response = call.awaitResponse()
+            if (response.isSuccessful) {
+                Log.d("Current Pokemon :", response.body().toString())
+                response.body()
+            } else {
+                throw Exception("Failed to fetch pokemon data")
             }
-            override fun onFailure(call: Call<Pokemon?>, t: Throwable) {
-                TODO("Not yet implemented")
-            }
-        })
+        } catch (e: Exception) {
+            Log.e("PokeAPI", "Error fetching pokemon data", e)
+            null
+        }
     }
 
 }
