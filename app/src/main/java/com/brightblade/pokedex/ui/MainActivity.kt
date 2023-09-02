4d0a24d818d7e5f25b922e4ce125c693a1ce8fd9
grann <fatrion.sa@gmail.com>
2023-09-02 15:28:38 +0200

    Added a condition where if the device navigates with system gestures the app is in full-screen, else it's not

--- app/src/main/java/com/brightblade/pokedex/ui/MainActivity.kt
+++ app/src/main/java/com/brightblade/pokedex/ui/MainActivity.kt
@@ -47,64 +47,71 @@ class MainActivity : AppCompatActivity() {
     private fun setUpBottomNavBar() {
         navHost =
             (supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment)
-        WindowCompat.setDecorFitsSystemWindows(this@MainActivity.window, false)
         WindowInsetsControllerCompat(
             window,
             window.decorView.findViewById(android.R.id.content)
         ).let { controller ->
-            controller.hide(WindowInsetsCompat.Type.navigationBars())
             controller.systemBarsBehavior =
                 WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
-
-        navHost.navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.id == R.id.pokeDetailsFragment2) {
-                binding.bottomNavView.animate().translationY(200f).setDuration(200).withEndAction {
-                    binding.bottomNavView.visibility = View.GONE
-                }.start()
-            } else
-                binding.bottomNavView.animate().apply {
-                    binding.bottomNavView.visibility = View.VISIBLE
-                    translationY(0f)
-                    duration = 200
-                }.start()
-        }
-        binding.bottomNavView.setOnTabSelectListener(object :
-            AnimatedBottomBar.OnTabSelectListener {
-            override fun onTabSelected(
-                lastIndex: Int,
-                lastTab: AnimatedBottomBar.Tab?,
-                newIndex: Int,
-                newTab: AnimatedBottomBar.Tab,
-            ) {
-                when (newIndex) {
-                    0 -> navHost.navController.navigate(
-                        resId = R.id.homeFragment,
-                        args = null,
-                        navOptions = NavOptions.Builder().setEnterAnim(R.anim.slide_in_left)
-                            .setExitAnim(R.anim.slide_out_right).build()
-                    )
-
-                    1 -> navHost.navController.navigate(
-                        resId = R.id.favoritePokemons,
-                        args = null,
-                        navOptions = NavOptions.Builder().setEnterAnim(R.anim.slide_in_right)
-                            .setExitAnim(R.anim.slide_out_left).build()
-                    )
+            controller.addOnControllableInsetsChangedListener { controller, typeMask ->
+                if (typeMask and WindowInsetsCompat.Type.navigationBars()
+                    == WindowInsetsCompat.Type.systemGestures()
+                ) {
+                    WindowCompat.setDecorFitsSystemWindows(this@MainActivity.window, false)
                 }
             }
 
-            // An optional method that will be fired whenever an already selected tab has been selected again.
-            override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
-                when (index) {
-                    0 -> {
-                        tabReselectedListener =
-                            navHost.childFragmentManager.fragments[0] as? HomeFragment
-                        tabReselectedListener?.onHomeButtonReSelected()
+            navHost.navController.addOnDestinationChangedListener { _, destination, _ ->
+                if (destination.id == R.id.pokeDetailsFragment2) {
+                    binding.bottomNavView.animate().translationY(200f).setDuration(200)
+                        .withEndAction {
+                            binding.bottomNavView.visibility = View.GONE
+                        }.start()
+                } else
+                    binding.bottomNavView.animate().apply {
+                        binding.bottomNavView.visibility = View.VISIBLE
+                        translationY(0f)
+                        duration = 200
+                    }.start()
+            }
+            binding.bottomNavView.setOnTabSelectListener(object :
+                AnimatedBottomBar.OnTabSelectListener {
+                override fun onTabSelected(
+                    lastIndex: Int,
+                    lastTab: AnimatedBottomBar.Tab?,
+                    newIndex: Int,
+                    newTab: AnimatedBottomBar.Tab,
+                ) {
+                    when (newIndex) {
+                        0 -> navHost.navController.navigate(
+                            resId = R.id.homeFragment,
+                            args = null,
+                            navOptions = NavOptions.Builder().setEnterAnim(R.anim.slide_in_left)
+                                .setExitAnim(R.anim.slide_out_right).build()
+                        )
+
+                        1 -> navHost.navController.navigate(
+                            resId = R.id.favoritePokemons,
+                            args = null,
+                            navOptions = NavOptions.Builder().setEnterAnim(R.anim.slide_in_right)
+                                .setExitAnim(R.anim.slide_out_left).build()
+                        )
                     }
                 }
-            }
-        })
+
+                // An optional method that will be fired whenever an already selected tab has been selected again.
+                override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+                    when (index) {
+                        0 -> {
+                            tabReselectedListener =
+                                navHost.childFragmentManager.fragments[0] as? HomeFragment
+                            tabReselectedListener?.onHomeButtonReSelected()
+                        }
+                    }
+                }
+            })
+
+        }
     }
 
     interface OnHomeButtonReselected {
